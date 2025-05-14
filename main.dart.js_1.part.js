1ea7814e25cb1bce8cc3bf59cea7a0bcbf347348
rn7cvj-dvfu <rn7cvj-dvfu@users.noreply.github.com>
2025-05-14 06:40:05 +0000

    deploy: 147cc80ca54e504a8d0118e52c5ee73476e47434

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -2,7 +2,7 @@
 a[b][c]=a[b][c]||[]
 a[b][c].push({p:"main.dart.js_1",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={
-awD(d,e,f){var y=new B.zN(),x=new A.Oc(y)
+awD(d,e,f){var y=new B.zO(),x=new A.Ob(y)
 y.r=A.aKB(x)
 return x},
 aKA(d,e){switch(e){case"strings.settings":return"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438"
@@ -10,21 +10,21 @@ case"strings.statistic":return"\u0421\u0442\u0430\u0442\u0438\u0441\u0442\u0438\
 case"strings.back":return"\u041d\u0430\u0437\u0430\u0434"
 default:return null}},
 aKB(d){return new A.amP(d)},
-Oc:function Oc(d){this.a=d
+Ob:function Ob(d){this.a=d
 this.c=this.b=$},
 am6:function am6(){},
 amP:function amP(d){this.a=d}}
 B=c[0]
 A=a.updateHolder(c[3],A)
-A.Oc.prototype={
+A.Ob.prototype={
 j(d,e){return this.a.FB(e)},
-gtv(){var y,x=this,w=x.c
+gtu(){var y,x=this,w=x.c
 if(w===$){y=x.b
 if(y===$){y!==$&&B.a8()
 x.b=x}w!==$&&B.a8()
 w=x.c=new A.am6()}return w},
-$ifD:1,
-$ifO:1}
+$ifC:1,
+$ifN:1}
 A.am6.prototype={
 gG1(){return"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438"},
 gGa(){return"\u0421\u0442\u0430\u0442\u0438\u0441\u0442\u0438\u043a\u0430"},
@@ -33,8 +33,8 @@ var z=a.updateTypes([])
 A.amP.prototype={
 $1(d){return A.aKA(this.a,d)},
 $S:73};(function inheritance(){var y=a.inheritMany,x=a.inherit
-y(B.C,[A.Oc,A.am6])
+y(B.C,[A.Ob,A.am6])
 x(A.amP,B.jf)})()
-B.axr(b.typeUniverse,JSON.parse('{"Oc":{"fD":[],"fO":["fd","fD"]}}'))};
+B.axr(b.typeUniverse,JSON.parse('{"Ob":{"fC":[],"fN":["fd","fC"]}}'))};
 ((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"zJBp7QO3jnCLgFyo07NWis85sH8=");
+a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"m5kuB92SxMw4SaDlGMkwz6EScPU=");
