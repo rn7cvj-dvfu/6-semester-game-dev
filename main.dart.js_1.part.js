215eeb9052dcdaf2d54920f63a6075959122a333
rn7cvj-dvfu <rn7cvj-dvfu@users.noreply.github.com>
2025-05-16 09:49:38 +0000

    deploy: 9e82980a45da3f1a3b7cf8bfc567107fd0960dfd

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -2,43 +2,43 @@
 a[b][c]=a[b][c]||[]
 a[b][c].push({p:"main.dart.js_1",e:"beginPart"})})(self,"$__dart_deferred_initializers__","eventLog")
 $__dart_deferred_initializers__.current=function(a,b,c,$){var B,A={
-az8(d,e,f){var y=new B.AG(),x=new A.Pk(y)
-y.r=A.aN8(x)
+azn(d,e,f){var y=new B.AJ(),x=new A.Po(y)
+y.r=A.aNp(x)
 return x},
-aN7(d,e){switch(e){case"strings.settings":return"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438"
+aNo(d,e){switch(e){case"strings.settings":return"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438"
 case"strings.statistic":return"\u0421\u0442\u0430\u0442\u0438\u0441\u0442\u0438\u043a\u0430"
 case"strings.back":return"\u041d\u0430\u0437\u0430\u0434"
 case"strings.useDarkTheme":return"\u0422\u0435\u043c\u043d\u0430\u044f \u0442\u0435\u043c\u0430"
 case"strings.useSeedColor":return"\u041e\u0441\u043d\u043e\u0432\u043d\u043e\u0439 \u0446\u0432\u0435\u0442"
 default:return null}},
-aN8(d){return new A.ap7(d)},
-Pk:function Pk(d){this.a=d
+aNp(d){return new A.apl(d)},
+Po:function Po(d){this.a=d
 this.c=this.b=$},
-aop:function aop(){},
-ap7:function ap7(d){this.a=d}}
+aoD:function aoD(){},
+apl:function apl(d){this.a=d}}
 B=c[0]
 A=a.updateHolder(c[3],A)
-A.Pk.prototype={
-i(d,e){return this.a.GJ(e)},
-gnt(){var y,x=this,w=x.c
+A.Po.prototype={
+i(d,e){return this.a.GN(e)},
+gnw(){var y,x=this,w=x.c
 if(w===$){y=x.b
 if(y===$){y!==$&&B.a8()
 x.b=x}w!==$&&B.a8()
-w=x.c=new A.aop()}return w},
-$ifP:1,
-$ih_:1}
-A.aop.prototype={
-gH8(){return"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438"},
-gHg(){return"\u0421\u0442\u0430\u0442\u0438\u0441\u0442\u0438\u043a\u0430"},
-gPn(){return"\u041d\u0430\u0437\u0430\u0434"},
-gTK(){return"\u0422\u0435\u043c\u043d\u0430\u044f \u0442\u0435\u043c\u0430"},
-gTL(){return"\u041e\u0441\u043d\u043e\u0432\u043d\u043e\u0439 \u0446\u0432\u0435\u0442"}}
+w=x.c=new A.aoD()}return w},
+$ifQ:1,
+$ih1:1}
+A.aoD.prototype={
+gHc(){return"\u041d\u0430\u0441\u0442\u0440\u043e\u0439\u043a\u0438"},
+gHk(){return"\u0421\u0442\u0430\u0442\u0438\u0441\u0442\u0438\u043a\u0430"},
+gPs(){return"\u041d\u0430\u0437\u0430\u0434"},
+gTQ(){return"\u0422\u0435\u043c\u043d\u0430\u044f \u0442\u0435\u043c\u0430"},
+gTR(){return"\u041e\u0441\u043d\u043e\u0432\u043d\u043e\u0439 \u0446\u0432\u0435\u0442"}}
 var z=a.updateTypes([])
-A.ap7.prototype={
-$1(d){return A.aN7(this.a,d)},
+A.apl.prototype={
+$1(d){return A.aNo(this.a,d)},
 $S:75};(function inheritance(){var y=a.inheritMany,x=a.inherit
-y(B.E,[A.Pk,A.aop])
-x(A.ap7,B.jA)})()
-B.azZ(b.typeUniverse,JSON.parse('{"Pk":{"fP":[],"h_":["fn","fP"]}}'))};
+y(B.E,[A.Po,A.aoD])
+x(A.apl,B.jC)})()
+B.aAd(b.typeUniverse,JSON.parse('{"Po":{"fQ":[],"h1":["fn","fQ"]}}'))};
 ((a,b)=>{a[b]=a.current
-a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"ujKFhUa54Ex3X1JgeIc6F07WHbU=");
+a.eventLog.push({p:"main.dart.js_1",e:"endPart",h:b})})($__dart_deferred_initializers__,"WWVbjSlIApEKg9WHrYrGFilLtF8=");
