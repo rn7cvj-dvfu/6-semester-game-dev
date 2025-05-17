24a81f853dc4c04410263013554d92e7046cefcd
rn7cvj-dvfu <rn7cvj-student@outlook.com>
2025-05-17 20:35:39 +1000

    refactor: replace Card with ElevatedButton for back button UI

--- game/lib/features/ui/back_button.dart
+++ game/lib/features/ui/back_button.dart
@@ -9,21 +9,30 @@ class GBackButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      clipBehavior: Clip.antiAlias,
-      child: InkWell(
-        onTap: onTap,
-        child: Padding(
-          padding: EdgeInsets.all(8),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Icon(Icons.arrow_back_ios_new, size: 16),
-              Text(context.t.strings.common.back),
-            ],
-          ),
-        ),
+    return ElevatedButton.icon(
+      onPressed: onTap,
+      label: Text(context.t.strings.common.back),
+      icon: const Icon(Icons.arrow_back_ios_new),
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
       ),
     );
+
+    // return Card(
+    //   clipBehavior: Clip.antiAlias,
+    //   child: InkWell(
+    //     onTap: onTap,
+    //     child: Padding(
+    //       padding: EdgeInsets.all(8),
+    //       child: Row(
+    //         mainAxisSize: MainAxisSize.min,
+    //         children: [
+    //           Icon(Icons.arrow_back_ios_new, size: 16),
+    //           Text(context.t.strings.common.back),
+    //         ],
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
