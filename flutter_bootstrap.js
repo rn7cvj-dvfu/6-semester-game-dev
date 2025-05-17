2adc36b209fc271470763e82a49208139db1d3b6
TheLastFlame <TheLastFlame@users.noreply.github.com>
2025-05-17 08:41:26 +0000

    deploy: 16631062bbde2706e42036fc85b0afc0cda85e9b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a682cf27b2372e1e8f82792316b05b3f6869c8
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1000675200"
+    serviceWorkerVersion: "2462048073"
   }
 });
